e10300d66f7abde343871a8a5996c42802795ff8
Mikko Heikkinen <hawk@biomi.org>
2019-02-28 00:45:49 +0100

    Send messages to Telegram channel, settings in env file

---
+++ www/config/env.template.php
@@ -0,0 +1,7 @@
+<?php
+
+define("LAJIFI_TOKEN",        "your api.laji.fi token");
+define("TELEGRAM_BOT_TOKEN",  "your bot token");
+define("TELEGRAM_CHAT_ID",    "@your chat_id");
+define("CRONITOR_PING_ID",    "your ping-id");
+define("LATESTID_FILENAME",   "latestid.dev.txt");
--- www/index.php
+++ www/index.php
@@ -1,2 +1,24 @@
 <?php
-phpinfo();
+//phpinfo();
+
+include "config/env.php";
+
+$message = "Test message ÅÄÖåäö";
+
+$url = "https://api.telegram.org/bot" . TELEGRAM_BOT_TOKEN . "/sendMessage?chat_id=" . TELEGRAM_CHAT_ID . "&text=$message";
+
+$response = json_decode(file_get_contents($url), TRUE);
+
+
+if ($response['ok']) {
+  echo "Success";
+}
+else {
+  echo "Failure";
+}
+echo "<pre>";
+print_r($response);
+echo $url;
+
+
+
