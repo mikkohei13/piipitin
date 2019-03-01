1fe4af9bd2963d79c65139da510588ebf2d71840
Mikko Heikkinen <hawk@biomi.org>
2019-03-01 16:23:19 +0200

    Add more compleq unit query

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Tools for sending data from FinBIF systems to mobile devices
 
 ## Todo
 
+- To show only fresh observations, add time filter. Now returns also old observations entred today.
 - Docker: Do not run Composer as root/super user! See https://getcomposer.org/root for details
 
 
@@ -24,6 +25,11 @@ Tools for sending data from FinBIF systems to mobile devices
 Example queries to api.laji.fi
 
 
+HR.1747 Kokoelma Vihko
+ML.206 Maa Suomi
+
+
+https://api.laji.fi/v0/warehouse/query/list?selected=document.collectionId%2Cdocument.createdDate%2Cdocument.documentId%2Cdocument.editorUserIds%2Cdocument.firstLoadDate%2Cdocument.formId%2Cdocument.loadDate%2Cdocument.modifiedDate%2Cdocument.sourceId%2Cgathering.biogeographicalProvince%2Cgathering.conversions.wgs84CenterPoint.lat%2Cgathering.conversions.wgs84CenterPoint.lon%2Cgathering.country%2Cgathering.displayDateTime%2Cgathering.eventDate.begin%2Cgathering.gatheringId%2Cgathering.interpretations.coordinateAccuracy%2Cgathering.linkings.observers.fullName%2Cgathering.locality%2Cgathering.municipality%2Cgathering.team%2Cunit.linkings.taxon.finnish%2Cunit.linkings.taxon.scientificName%2Cunit.reportedTaxonId%2Cunit.taxonVerbatim%2Cunit.unitId&orderBy=document.firstLoadDate&pageSize=20&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&collectionId=HR.1747&individualCountMin=1&firstLoadedSameOrAfter=2019-02-28%20DESC&qualityIssues=NO_ISSUES&access_token=
 
 /v0/warehouse/query/list?selected=document.createdDate%2Cdocument.documentId%2Cdocument.loadDate%2Cgathering.biogeographicalProvince%2Cgathering.conversions.wgs84CenterPoint.lat%2Cgathering.conversions.wgs84CenterPoint.lon%2Cgathering.country%2Cgathering.eventDate.begin%2Cgathering.eventDate.end%2Cgathering.gatheringId%2Cgathering.interpretations.biogeographicalProvince%2Cgathering.interpretations.country%2Cgathering.interpretations.finnishMunicipality%2Cgathering.locality%2Cgathering.municipality%2Cgathering.notes%2Cgathering.province%2Cgathering.team%2Cunit.linkings.taxon.qname%2Cunit.linkings.taxon.scientificName%2Cunit.linkings.taxon.vernacularName%2Cunit.unitId&orderBy=document.documentId%20DESC&pageSize=100&page=1&collectionId=HR.1747&access_token=
 
--- www/lajifi.php
+++ www/lajifi.php
@@ -6,8 +6,13 @@ function getDataFromLajifi($url) {
     return $responseJSON;
 }
 
-function buildListQuery($token) {
 
+/*
+https://api.laji.fi/v0/warehouse/query/list?pageSize=100&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&individualCountMin=1&qualityIssues=NO_ISSUES
+*/
+function buildListQueryOLD($token) {
+
+    // Selected fields
     $selectedArr = Array(
         "document.createdDate",
         "document.documentId",
@@ -33,49 +38,84 @@ function buildListQuery($token) {
         "unit.unitId"
     );
     $selected = join("%2C", $selectedArr);
+
+    // Settings
     $orderBy = "document.createdDate";
     $orderDirection = "DESC"; // DESC / ASC
     $limit = 10;
-    $collectionIdQname = "HR.1747";
 
-    $url = "https://api.laji.fi/v0/warehouse/query/list?selected=" . $selected . "&orderBy=" . $orderBy . "%20" . $orderDirection . "&pageSize=" . $limit . "&page=1&collectionId=" . $collectionIdQname . "&access_token=" . $token;
+    // Filters
+    $collectionIdQname = "HR.1747";
+    $countryIdQname = "ML.206";
+
+    $url = "https://api.laji.fi/v0/warehouse/query/list?selected=" . $selected . "&orderBy=" . $orderBy . "%20" . $orderDirection . "&pageSize=" . $limit . "&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&individualCountMin=1&qualityIssues=NO_ISSUES&collectionId=" . $collectionIdQname . "&countryId=" . $countryIdQname . "&access_token=" . $token;
+
+ //   $url = "https://api.laji.fi/v0/warehouse/query/list?selected=" . $selected . "&orderBy=" . $orderBy . "%20" . $orderDirection . "&pageSize=" . $limit . "&page=1&collectionId=" . $collectionIdQname . "&access_token=" . $token; // OLD from Telelaitos
 
     return $url;
 }
 
-function buildAggregateQuery($token) {
+/*
+https://api.laji.fi/v0/warehouse/query/list?selected=document.collectionId%2Cdocument.createdDate%2Cdocument.documentId%2Cdocument.editorUserIds%2Cdocument.firstLoadDate%2Cdocument.formId%2Cdocument.loadDate%2Cdocument.modifiedDate%2Cdocument.sourceId%2Cgathering.biogeographicalProvince%2Cgathering.conversions.wgs84CenterPoint.lat%2Cgathering.conversions.wgs84CenterPoint.lon%2Cgathering.country%2Cgathering.displayDateTime%2Cgathering.eventDate.begin%2Cgathering.gatheringId%2Cgathering.interpretations.coordinateAccuracy%2Cgathering.linkings.observers.fullName%2Cgathering.locality%2Cgathering.municipality%2Cgathering.team%2Cunit.linkings.taxon.finnish%2Cunit.linkings.taxon.scientificName%2Cunit.reportedTaxonId%2Cunit.taxonVerbatim%2Cunit.unitId&orderBy=document.firstLoadDate&pageSize=20&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&collectionId=HR.1747&individualCountMin=1&firstLoadedSameOrAfter=2019-02-28%20DESC&qualityIssues=NO_ISSUES&access_token=
+*/
 
+function buildListQuery($token) {
+
+    // Selected fields
     $selectedArr = Array(
+        "document.collectionId",
         "document.createdDate",
         "document.documentId",
+        "document.editorUserIds",
+        "document.firstLoadDate",
+        "document.formId",
         "document.loadDate",
+        "document.modifiedDate",
+        "document.sourceId",
         "gathering.biogeographicalProvince",
         "gathering.conversions.wgs84CenterPoint.lat",
         "gathering.conversions.wgs84CenterPoint.lon",
         "gathering.country",
+        "gathering.displayDateTime",
         "gathering.eventDate.begin",
-        "gathering.eventDate.end",
         "gathering.gatheringId",
-        "gathering.interpretations.biogeographicalProvince",
-        "gathering.interpretations.country",
-        "gathering.interpretations.finnishMunicipality",
+        "gathering.interpretations.coordinateAccuracy",
+        "gathering.linkings.observers.fullName",
         "gathering.locality",
         "gathering.municipality",
-        "gathering.notes",
-        "gathering.province",
         "gathering.team",
-        "unit.linkings.taxon.qname",
+        "unit.linkings.taxon.finnish",
         "unit.linkings.taxon.scientificName",
-        "unit.linkings.taxon.vernacularName",
+        "unit.reportedTaxonId",
+        "unit.taxonVerbatim",
         "unit.unitId"
     );
     $selected = join("%2C", $selectedArr);
-    $orderBy = "document.createdDate";
+
+    // Settings
+    $orderBy = "document.firstLoadDate";
     $orderDirection = "DESC"; // DESC / ASC
     $limit = 10;
-    $collectionIdQname = "HR.1747";
 
-    $url = "https://api.laji.fi/v0/warehouse/query/list?selected=" . $selected . "&orderBy=" . $orderBy . "%20" . $orderDirection . "&pageSize=" . $limit . "&page=1&collectionId=" . $collectionIdQname . "&access_token=" . $token;
+    // Filters
+    // To show only fresh observations, add time filter. Now returns also old observations entred today.
+
+    $collectionIdQname = "HR.1747";
+    $countryIdQname = "ML.206";
+    $date = date("Y-m-d");
+
+$url = "https://api.laji.fi/v0/warehouse/query/list?selected=" . $selected . "&orderBy=" . $orderBy . "%20" . $orderDirection . "&pageSize=20&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&collectionId=" . $collectionIdQname . "&countryId=" . $countryIdQname . "&firstLoadedSameOrAfter=" . $date . "&qualityIssues=NO_ISSUES&access_token=" . $token;
+
+//    $url = "https://api.laji.fi/v0/warehouse/query/list?selected=" . $selected . "&orderBy=" . $orderBy . "%20" . $orderDirection . "&pageSize=" . $limit . "&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&individualCountMin=1&qualityIssues=NO_ISSUES&collectionId=" . $collectionIdQname . "&countryId=" . $countryIdQname . "&access_token=" . $token;
+
+    return $url;
+}
+
+function buildAggregateQuery($token, $date) {
+
+    // Date has to be in format 2018-12-24
+
+    $url = "https://api.laji.fi/v0/warehouse/query/aggregate?aggregateBy=document.collectionId&geoJSON=false&onlyCount=true&pairCounts=false&excludeNulls=true&pessimisticDateRangeHandling=false&pageSize=100&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&individualCountMin=1&firstLoadedSameOrAfter=" . $date . "&qualityIssues=NO_ISSUES&access_token=" . $token;
 
     return $url;
 }
