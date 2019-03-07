1f5b6022692608a609cfc8945487976ad50e6f87
Mikko Heikkinen <hawk@biomi.org>
2019-03-07 10:56:12 +0200

    Add viewer file

---
+++ www/view/index.php
@@ -0,0 +1,41 @@
+<h1>Viimeksi tallennetut "mielenkiintoiset" havainnot Lajitietokeskuksesta (DEMO)</h2>
+
+<?php
+/*
+- sort descending
+- limit 50
+- show points & details etc.
+*/
+
+require_once "../config/env.php";
+
+
+$dataJson = file_get_contents("../data/" . DATAFILE_RARITIES);
+$dataArr = json_decode($dataJson, TRUE);
+
+$observationCount = 0;
+
+foreach ($dataArr as $element) {
+  echo "<p>\n</strong>";
+  echo @$element['unit']['linkings']['taxon']['vernacularName']['fi'] . " (<em>";
+  echo @$element['unit']['linkings']['taxon']['scientificName'] . "</em>)</strong><br>\n";
+  echo @$element['gathering']['displayDateTime'] . "<br>\n";
+  echo @$element['gathering']['biogeographicalProvince'] . ", ";
+  echo @$element['gathering']['municipality'] . ", ";
+  echo @$element['gathering']['locality'] . "<br>\n";
+ 
+  if (isset($element['gathering']['team'])) {
+    foreach ($element['gathering']['team'] as $i => $name) {
+      echo $name . ", ";
+    }
+  }
+  echo "<br>\n";
+
+  echo "<a href=\"" . $element['document']['documentId'] . "\">" . $element['document']['documentId'] . "</a>\n";
+
+  echo "<p>\n\n";
+
+  $observationCount++;
+}
+
+echo "<p>Yhteensä $observationCount havaintoa</p>";
