e3639433bc303211a909cbc948f44104af199c81
Mikko Heikkinen <hawk@biomi.org>
2019-03-29 08:20:26 +0200

    Add fields, sort by date

--- www/top/scarce.php
+++ www/top/scarce.php
@@ -1,12 +1,11 @@
 <h1>Uusia lajeja Suomesta</h1>
 <p></p>
 <hr>
-<pre>
 <?php
 
 //$namesObservations = Array();
 
-$url = "https://api.laji.fi/v0/warehouse/query/aggregate?aggregateBy=unit.linkings.originalTaxon.id&orderBy=count&geoJSON=false&onlyCount=true&pairCounts=false&excludeNulls=true&pessimisticDateRangeHandling=false&pageSize=100&page=1&cache=false&taxonId=MX.53695&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&taxonRankId=MX.species&countryId=ML.206&individualCountMin=1&qualityIssues=NO_ISSUES&access_token=" . LAJIFI_TOKEN;
+$url = "https://api.laji.fi/v0/warehouse/query/aggregate?aggregateBy=unit.linkings.originalTaxon.id&orderBy=count&geoJSON=false&onlyCount=true&pairCounts=false&excludeNulls=true&pessimisticDateRangeHandling=false&pageSize=100&page=1&cache=false&taxonId=MX.53695&useIdentificationAnnotations=false&includeSubTaxa=true&includeNonValidTaxa=false&taxonRankId=MX.species&countryId=ML.206&individualCountMin=1&qualityIssues=NO_ISSUES&access_token=" . LAJIFI_TOKEN;
 
 $speciesJson = getDataFromLajifi($url);
 $speciesArr = json_decode($speciesJson, TRUE);
@@ -14,8 +13,11 @@ $speciesArr = json_decode($speciesJson, TRUE);
 $threshold = 1;
 $limit = 20;
 
-$limit = 2; // debug
+$limit = 100; // debug
 
+$obsArray = Array();
+
+// Build observations with related data into an array
 foreach ($speciesArr['results'] as $i => $species) {
   if ($species['count'] > $threshold) {
     break;
@@ -26,14 +28,37 @@ foreach ($speciesArr['results'] as $i => $species) {
 
   $taxonId = $species['aggregateBy']['unit.linkings.originalTaxon.id'];
   $taxonData = getTaxonData($taxonId);
-  $obs = getObservationUnitId($taxonId);
+  $obs = getObservationUnit($taxonId);
+  $combination = Array();
 
-  // debug:
-  echo $taxonData['family'] . ": <a href='" . $taxonId . "'>" . $taxonData['species'] . "</a> (" . $taxonData['speciesVernacular'] . ") <a href='" . $obs['unit']['unitId'] . "'>OBS</a>\n";
-  echo $obs['gathering']['displayDateTime'] . "\n\n";
+  $combination['taxonId'] = $taxonId;
+  $combination['taxon'] = $taxonData;
+  $combination['obs'] = $obs;
+
+//  print_r ($combination); // debug
+
+  $obsArray[] = $combination; // obsArr = ?
+
 }
 
-print_r ($speciesArr);
+// Sort by date
+usort($obsArray, function($b, $a) {
+  $retval = $a['obs']['gathering']['eventDate']['begin'] <=> $b['obs']['gathering']['eventDate']['begin'];
+  return $retval;
+});
+  
+// Echo each obs
+foreach ($obsArray as $a => $obs) {
+  echo "<p><strong>";
+  // debug:
+  echo $obs['taxon']['family'] . ": <a href='" . $obs['taxonId'] . "'><em>" . $obs['taxon']['species'] . "</em></a> (" . $obs['taxon']['speciesVernacular'] . ") <a href='" . $obs['obs']['unit']['unitId'] . "'>OBS</a></strong><br>\n";
+  echo $obs['obs']['gathering']['displayDateTime'] . "<br>\n";
+  echo $obs['obs']['gathering']['province'] . " " . $obs['obs']['gathering']['municipality'] . " " . $obs['obs']['gathering']['locality'] . "<br>\n";
+  echo $obs['obs']['teamString'] . "<br>\n";
+  echo "</p>\n\n";
+}
+
+//print_r ($speciesArr);
 
 //arsort($namesObservations);
 
@@ -41,20 +66,41 @@ print_r ($speciesArr);
 
 
 
-function getObservationUnitId($taxonId) {
+function getObservationUnit($taxonId) {
   /*
  https://api.laji.fi/v0/warehouse/query/list?pageSize=10&page=1&cache=false&taxonId=MX.204861&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&individualCountMin=1&qualityIssues=NO_ISSUES&access_token=
   */
 
   $taxonQname = str_replace("http://tun.fi/", "", $taxonId);
 
-  $url = "https://api.laji.fi/v0/warehouse/query/list?pageSize=10&page=1&cache=false&taxonId=" . $taxonQname . "&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&individualCountMin=1&qualityIssues=NO_ISSUES&access_token=" . LAJIFI_TOKEN;
+  $url = "https://api.laji.fi/v0/warehouse/query/list?selected=unit.unitId%2Cgathering.country%2Cgathering.displayDateTime%2Cgathering.eventDate.begin%2Cgathering.eventDate.end%2Cgathering.locality%2Cgathering.municipality%2Cgathering.province%2Cgathering.team%2Cunit.abundanceString%2Cunit.annotationCount%2Cunit.linkings.originalTaxon.finnish%2Cunit.linkings.originalTaxon.id%2Cunit.linkings.originalTaxon.scientificName&pageSize=2&page=1&cache=false&taxonId=" . $taxonQname . "&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&individualCountMin=1&qualityIssues=NO_ISSUES&access_token=" . LAJIFI_TOKEN;
   $obsJson = getDataFromLajifi($url);
   $obsArr = json_decode($obsJson, TRUE);
 
   // Todo: check that really only 1 result
 
-  return $obsArr['results'][0];
+  $ret = $obsArr['results'][0];
+
+  if (!isset($ret['gathering']['province'])) {
+    $ret['gathering']['province'] = "";
+  }
+  if (!isset($ret['gathering']['municipality'])) {
+    $ret['gathering']['municipality'] = "";
+  }
+  if (!isset($ret['gathering']['locality'])) {
+    $ret['gathering']['locality'] = "";
+  }
+
+  // Todo: with a function
+  $teamString = "";
+  foreach ($ret['gathering']['team'] as $i => $person) {
+    $teamString .= $person . ", ";
+  }
+  $ret['teamString'] = trim($teamString, ", ");
+
+//  print_r($ret);
+
+  return $ret;
 }
 
 function getTaxonData($taxonId) {
