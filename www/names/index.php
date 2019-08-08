395c13d9f262e676c4dc4dc54731b213a4521ba0
Mikko Heikkinen <hawk@biomi.org>
2019-08-08 20:40:12 +0300

    Fetch names

--- www/names/index.php
+++ www/names/index.php
@@ -1,101 +1,110 @@
 <?php
+/*
+Notes:
+- default page size seems to be 100, even though docs say its 20. If no page size is set, pagination metadata is incorrect.
+
+What are names that contain parenthesis? Subgenera? e.g. 
+http://tun.fi/MX.228146
+http://tun.fi/MX.228153
+*/
 
 require_once "../config/env.php";
 
-define("DEBUG", true);
+define("DEBUG", false);
 
+$params = Array();
 
-$url = buildListQuery("ML.206"); // Only Finnish
-$dataJSON = getDataFromLajifi($url);
-$dataArr = json_decode($dataJSON, TRUE);
+$params['pageSize'] = 100;
+$params['page'] = 1;
 
-print_r ($dataArr);
+$pageLimit = "10"; // DEBUG
 
+$morePages = TRUE;
+
+while($morePages) {
+    $url = buildTaxonQuery($params);
+    $dataJSON = getDataFromLajifi($url);
+    $dataArr = json_decode($dataJSON, TRUE);
+
+    // Do something with $dataArr
+    printNames($dataArr['results']);
+
+    if ($dataArr['currentPage'] >= $dataArr['lastPage']) {
+        // Last page, stop processing
+        $morePages = FALSE;
+    }
+    elseif ($dataArr['currentPage'] >= $pageLimit) {
+        // Reached limit, stop processing
+        $morePages = FALSE;
+    }
+    else {
+        // Get next page
+        $params['page']++;
+    }
+}
+
+function printNames($results) {
+    foreach ($results as $nameKey => $nameArr) {
+        debugData($nameArr, __LINE__);
+
+        // Synonyms
+        $synonyms = "";
+        if (isset($nameArr['synonyms'])) {
+            foreach ($nameArr['synonyms'] as $synonymKey => $synonymArr) {
+                $synonyms = $synonyms . "\t" . $synonymArr['scientificName'];
+            }
+            $synonyms = trim($synonyms);
+        }
+
+        // If no vernacular name, replace with empty
+        if (!isset($nameArr['vernacularName'])) {
+            $nameArr['vernacularName'] = "";
+        }
+
+        echo "http://tun.fi/" . $nameArr['id'] . "\t" . $nameArr['scientificName'] . "\t" . $nameArr['vernacularName'] . "\t" . $synonyms . "\n";
+    }
+}
 
 // ------------------------------------------------
-
-/*
-https://api.laji.fi/v0/taxa?lang=multi&langFallback=false&includeHidden=false&includeMedia=false&includeDescriptions=false&includeRedListEvaluations=false&sortOrder=taxonomic&page=1&pageSize=100&access_token=G5XyRIgUAQdvug0CTaprxD2GUNXjaT10JEI4W4gbqZb2y7IZHDiNWnkusvtBp1R2
-*/
 
 // Get data from an api and return as JSON
 function getDataFromLajifi($url) {
     $responseJSON = file_get_contents($url);
     if ($responseJSON === FALSE) {
         $errorMessage = "Error fetching data from api.laji.fi";
-        logger("lajifi.log", "error", $errorMessage);
+//        logger("lajifi.log", "error", $errorMessage);
         exit($errorMessage);
     }
     return $responseJSON;
 }
 
+
 /*
-Build and return query URL for a list of units.
+Build and return query URL for a list of taxa
 
-https://api.laji.fi/v0/warehouse/query/list?selected=document.collectionId%2Cdocument.createdDate%2Cdocument.documentId%2Cdocument.editorUserIds%2Cdocument.firstLoadDate%2Cdocument.formId%2Cdocument.loadDate%2Cdocument.modifiedDate%2Cdocument.sourceId%2Cgathering.biogeographicalProvince%2Cgathering.conversions.wgs84CenterPoint.lat%2Cgathering.conversions.wgs84CenterPoint.lon%2Cgathering.country%2Cgathering.displayDateTime%2Cgathering.eventDate.begin%2Cgathering.gatheringId%2Cgathering.interpretations.coordinateAccuracy%2Cgathering.linkings.observers.fullName%2Cgathering.locality%2Cgathering.municipality%2Cgathering.team%2Cunit.linkings.taxon.finnish%2Cunit.linkings.taxon.scientificName%2Cunit.reportedTaxonId%2Cunit.taxonVerbatim%2Cunit.unitId&orderBy=document.firstLoadDate&pageSize=20&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&collectionId=HR.1747&individualCountMin=1&firstLoadedSameOrAfter=2019-02-28%20DESC&qualityIssues=NO_ISSUES&access_token=
+MH:
+https://api.laji.fi/v0/taxa?lang=multi&langFallback=false&includeHidden=false&includeMedia=false&includeDescriptions=false&includeRedListEvaluations=false&sortOrder=taxonomic&page=1&pageSize=100&access_token=
+
+VR:
+https://laji.fi/api/taxa?selectedFields=vernacularName,id,scientificName,synonyms.scientificName&lang=fi
 */
-function buildListQuery($countryIdQname = "") {
-
-    // Selected fields
-    $selectedArr = Array(
-        "document.collectionId",
-        "document.createdDate",
-        "document.documentId",
-        "document.editorUserIds",
-        "document.firstLoadDate",
-        "document.formId",
-        "document.loadDate",
-        "document.modifiedDate",
-        "document.sourceId",
-        "gathering.biogeographicalProvince",
-        "gathering.conversions.wgs84CenterPoint.lat",
-        "gathering.conversions.wgs84CenterPoint.lon",
-        "gathering.conversions.year",
-        "gathering.conversions.dayOfYearBegin",
-        "gathering.conversions.dayOfYearEnd",
-        "gathering.country",
-        "gathering.displayDateTime",
-        "gathering.eventDate.begin",
-        "gathering.gatheringId",
-        "gathering.interpretations.coordinateAccuracy",
-        "gathering.linkings.observers.fullName",
-        "gathering.locality",
-        "gathering.municipality",
-        "gathering.team",
-        "unit.linkings.taxon.id",
-        "unit.linkings.taxon.qname",
-        "unit.linkings.taxon.finnish",
-        "unit.linkings.taxon.scientificName",
-        "unit.linkings.taxon.vernacularName",
-        "unit.linkings.taxon.taxonRank",
-        "unit.reportedTaxonId",
-        "unit.taxonVerbatim",
-        "unit.unitId"
-    );
-    $selected = join("%2C", $selectedArr);
-
-    // Settings
-    $orderBy = "document.firstLoadDate";
-    $orderDirection = "DESC"; // DESC / ASC
-
-    if (DEBUG) {
-        $limit = 50;
-    }
-    else {
-        $limit = 50;
-    }
-
-    // Filters
-    // To show only fresh observations, add time filter. Now returns also old observations entred today.
-
-    $collectionIdQname = "HR.1747";
-    $date = date("Y-m-d");
-
-    $url = "https://api.laji.fi/v0/warehouse/query/list?selected=" . $selected . "&orderBy=" . $orderBy . "%20" . $orderDirection . "&pageSize=" . $limit . "&page=1&cache=false&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&collectionId=" . $collectionIdQname . "&countryId=" . $countryIdQname . "&firstLoadedSameOrAfter=" . $date . "&qualityIssues=NO_ISSUES&access_token=" . LAJIFI_TOKEN;
-
-    echo "URL built:\n</pre>" . $url . "<pre>\n"; // debug
-
+function buildTaxonQuery($params) {
+    $url = "https://laji.fi/api/taxa?selectedFields=vernacularName,id,scientificName,synonyms.scientificName&lang=fi&pageSize=" . $params['pageSize'] . "&page=" . $params['page'];
+    debugData($url, __LINE__);
     return $url;
+}
+
+function debugData($data, $line = "?") {
+    if (DEBUG) {
+        if (is_array($data)) {
+            echo "<pre>LINE $line: \n";
+            print_r ($data);
+            echo "\n\n";
+        }
+        else {
+            echo "<pre>LINE $line: $data \n\n";
+        }
+    }
 }
 
 
