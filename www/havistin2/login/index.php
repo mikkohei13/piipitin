513328b8636860121efdd126ee3a64a358a66b14
Mikko Heikkinen <hawk@biomi.org>
2019-10-29 18:15:47 +0200

    Add login page

--- www/havistin2/login/index.php
+++ www/havistin2/login/index.php
@@ -1,2 +1,3 @@
 <h1>Havistin</h1>
-<p><a href="">Login via Laji.fi</a></p>
+<p><a href="https://login.laji.fi/login?target=KE.781&redirectMethod=GET&locale=fi&next=/auth">Kirjaudu Havistimeen Laji.fi:n kautta</a></p>
+
