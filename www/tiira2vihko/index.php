07ba50878857963d54046015814b188b4bd66443
mikko <hawk@biomi.org>
2021-03-30 00:25:41 +0300

    Old changes

--- www/tiira2vihko/index.php
+++ www/tiira2vihko/index.php
@@ -9,6 +9,7 @@ if (isset($_GET['DEBUG'])) {
     <title>Muunna Tiira-havaintotaulukko Vihko-muotoon</title>
   </head>
   <body>
+  Tällä työkalulla voi muuntaa Tiira-lintutietojärjestelmästä haetun omien havaintojen havaintotiedoston muotoon, jossa sen voi tallentaa Lajitietokeskuksen Vihko-havaintopalveluun. Käyttö omalla vastuulla. Muunnettaavia tietoja ei tallenneta. Lue ohjeet: <a href="https://github.com/mikkohei13/piipitin/tree/master/www/tiira2vihko">https://github.com/mikkohei13/piipitin/tree/master/www/tiira2vihko</a> 
   <form action="<?php echo $action; ?>" method="post" enctype="multipart/form-data">
       Muunnettava Tiira-tiedosto:
       <input type="file" value="Valitse tiedosto" name="file" id="file">
